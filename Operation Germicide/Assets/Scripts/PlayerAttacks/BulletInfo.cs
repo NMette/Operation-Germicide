27b70c712cca8446f9e9a1773f67a9aba9e94ff6
NMETTE74 <nmette03@gmail.com>
2023-09-30 21:24:44 -0700

    Created Basic Bullet Scritps
    Setting up bullet scripts for spawning of bullets that can have different effects and stats.

---
+++ Operation Germicide/Assets/Scripts/PlayerAttacks/BulletInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletInfo
+{
+
+    private GameObject spawner;
+
+    private int damage;
+    
+    private int hitsLeft;
+    private int distanceTilDestroy;
+    private bool destroyOnHit;
+
+    private int speed;
+    
+
+}

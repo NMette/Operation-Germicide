679bd80f7fdc6f531f71aeb11e53c6941ca6025c
NMETTE74 <nmette03@gmail.com>
2023-10-01 19:45:09 -0700

    Further Work on Bullet Objects
    Worked on making scripts for bullets. Very slowly, but surely.

--- Operation Germicide/Assets/Scripts/PlayerAttacks/BarrelFire.cs
+++ Operation Germicide/Assets/Scripts/PlayerAttacks/BarrelFire.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BarrelFire : MonoBehaviour
 {
 
+    [SerializeField] private readonly float cooldown;
+    private float gunHeat;
 
+    private List<BulletInfo> bullets;
+    private float bulletForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullets = new();
+        gunHeat = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gunHeat > 0)
+        {
+            gunHeat -= Time.deltaTime;
+        } else if (Input.GetMouseButtonDown(0))
+        {
+            gunHeat = cooldown;
+            FireBullet();
+        }
+    }
+
+    void FireBullet()
+    {
         
     }
+
 }
--- Operation Germicide/Assets/Scripts/PlayerAttacks/BulletInfo.cs
+++ Operation Germicide/Assets/Scripts/PlayerAttacks/BulletInfo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletInfo
+public class BulletInfo : MonoBehaviour
 {
 
     private GameObject spawner;
@@ -10,10 +10,55 @@ public class BulletInfo
     private int damage;
     
     private int hitsLeft;
-    private int distanceTilDestroy;
+    private float distanceTilDestroy;
     private bool destroyOnHit;
 
-    private int speed;
-    
+    private float speed;
+
+    private string[] effects;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        this.speed = 0f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if collision has an enemy type, damage it
+
+        if (distanceTilDestroy <= 0f)
+        {
+            Destroy(gameObject);
+        }
+        if (destroyOnHit)
+        {
+            hitsLeft -= 1;
+            if (hitsLeft <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Setup(GameObject spawner, int damage, float distanceTilDestroy, bool destroyOnHit, float speed, string[] effects, int hitsLeft=1)
+    {
+        this.spawner = spawner;
+        this.damage = damage;
+        this.hitsLeft = hitsLeft;
+        this.distanceTilDestroy = distanceTilDestroy;
+        this.destroyOnHit = destroyOnHit;
+        this.speed = speed;
+        this.effects = effects;
+    }
+
+    public GameObject GetSpawner() { return spawner; }
+    public int GetDamage() { return damage; }
+    public int GetHitsLeft() { return hitsLeft; }
+    public float GetDistanceTilDestroy() {  return distanceTilDestroy; }
+    public float GetSpeed() { return speed; }
+    public string[] GetEffects() {  return effects; }
 
 }
